0d83fcd7e78e08f941623ff9cb936727ab968e8b
Rodrigo <ro-sk-8@hotmail.com>
2024-03-20 22:19:13 -0300

    01 - comentarios, variaveis, concatenação e interpolação

---
+++ 01/teste.php
@@ -0,0 +1,11 @@
+<?php
+    //echo "Olá mundo! <br>"
+
+   $nome = "Rodrigo";
+   $sobrenome = "Carvalho";
+   $nome_completo = $nome . " ". $sobrenome;
+
+
+    echo $nome ." ". $sobrenome;
+    echo $nome_completo;
+?>
